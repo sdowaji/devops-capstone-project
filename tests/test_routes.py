6582964eb9381d0cab46f23be6f66bd8f78fd53a
sdowaji <39618011+sdowaji@users.noreply.github.com>
2024-06-17 14:50:24 +0300

    Update test_routes.py
    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -21,9 +21,12 @@ DATABASE_URI = os.getenv(
 BASE_URL = "/accounts"
 HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 
+
 ######################################################################
 #  T E S T   C A S E S
 ######################################################################
+
+
 class TestAccountService(TestCase):
     """Account Service Tests"""
 
@@ -36,7 +39,6 @@ class TestAccountService(TestCase):
         talisman.force_https = False
         app.logger.setLevel(logging.CRITICAL)
         init_db(app)
-
 
     @classmethod
     def tearDownClass(cls):
@@ -141,6 +143,7 @@ class TestAccountService(TestCase):
         """It should not Read an Account that is not found"""
         resp = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+      
     def test_get_account_list(self):
         """It should Get a list of Accounts"""
         self._create_accounts(5)
